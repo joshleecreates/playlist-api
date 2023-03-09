2b7b3a99b94c587aff8734ebe3c7a15bc8bdcca7
Josh Lee <josh@joshuamlee.com>
2023-03-09 11:29:58 -0500

    adds locust

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,6 +1,7 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, constant_pacing
 
 class PlaylistUser(HttpUser):
+    wait_time = constant_pacing(10)
     @task
     def hello_world(self):
         self.client.get("/playlist/01")
