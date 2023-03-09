78c6bb1eb1cfd456a3425df17fd4472658440dd2
Josh Lee <josh@joshuamlee.com>
2023-03-09 17:33:01 -0500

    fix seeds

--- api/src/db.js
+++ api/src/db.js
@@ -5,13 +5,14 @@ import redis from "redis";
 const connectionString = process.env.MONGO_URL || "";
 mongoose.connect(connectionString);
 
-const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6317"
-const client = redis.createClient(redisUrl);
-client.on("error", function(error) {
-  console.error(`❗️ Redis Error: ${error}`)
-});
-
-mongooseCache(mongoose, client);
-client.connect();
+if(process.env.ENABLE_CACHE) {
+  const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6317"
+  const client = redis.createClient(redisUrl);
+  client.on("error", function(error) {
+    console.error(`❗️ Redis Error: ${error}`)
+  });
+  client.connect();
+  mongooseCache(mongoose, client);
+}
 
 export default mongoose;
