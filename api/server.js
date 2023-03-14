663f467f6aa56c62857e83dff07c12cf9b9b748f
Josh Lee <josh@joshuamlee.com>
2023-03-14 16:30:30 -0400

    well it works

--- api/server.js
+++ api/server.js
@@ -1,22 +1,25 @@
 'use strict';
 
-//Import express
-import instana from '@instana/collector';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const instana = require('@instana/collector')();
 import express from 'express';
 import { getPlaylist } from './src/playlists.js';
 import logger from './src/logger.js';
 import './src/db.js';
 
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-
 const app = express();
+
 app.use((req, res, next) => {
   req.logger = logger;
   next();
 });
+instana.setLogger(logger);
+
 app.get('/health', (req, res) => {
   var stat = {
       app: 'OK',
@@ -29,6 +32,7 @@ app.get('/playlist/:id', async (req, res) => {
   const data = await getPlaylist(req.params.id);
   res.json(data);
 });
+
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
